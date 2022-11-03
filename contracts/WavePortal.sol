2529dc39b267e8c88ef0f69f2783156d764a7334
Riellima <marielle.diandy@gmail.com>
2022-11-04 00:42:44 +0100

    Strore data on the smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -6,7 +6,19 @@ import "hardhat/console.sol";
 
 contract WavePortal{
 
+    uint totalWaves;
+
     constructor(){
         console.log("Wave portal contract");
+    }
+
+    function getTotalWaves() public view returns (uint) {
+        console.log("Total waves : %d", totalWaves);
+        return totalWaves;
+    }
+
+    function wave() public {
+        totalWaves++;
+        console.log("%s has waved", msg.sender);
     }
 }
