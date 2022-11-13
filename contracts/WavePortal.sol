63ad2ad84d6a43607f7a0da20e22d7e41ce54e9b
Riellima <marielle.diandy@gmail.com>
2022-11-13 18:12:24 +0100

    Wave function

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,24 +1,65 @@
-//SPDX-License-Identifier: UNLICENCED
+//SPDX-License-Identifier: UNLICENSED
 
-pragma solidity 0.8.17;
+pragma solidity  0.8.17;
 
 import "hardhat/console.sol";
 
-contract WavePortal{
+contract WavePortal {
 
+    mapping(address => uint) individualWavesCount;
+    mapping(address => uint) lastWaveTime;
     uint totalWaves;
+    Wave[] public waves ;
+    uint private seed;
+    
 
-    constructor(){
-        console.log("Wave portal contract");
+    struct Wave {
+        address waver;
+        string message;
+        uint timestamp;
     }
 
-    function getTotalWaves() public view returns (uint) {
-        console.log("Total waves : %d", totalWaves);
+    event NewWave(address indexed from, uint timestamp, string message);
+
+    constructor() payable {
+        console.log("Yo i am a contract called WavePortal that can receive waves!");
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+    function wave(string memory _msg) public{
+        require(lastWaveTime[msg.sender] + 15 minutes < block.timestamp, "Wait 15 minutes after your last wave");
+        individualWavesCount[msg.sender]++;
+        totalWaves++;
+
+        waves.push(Wave(msg.sender, _msg, block.timestamp));
+        console.log("%s has waved with : %s", msg.sender, _msg);
+        
+        emit NewWave(msg.sender, block.timestamp, _msg);
+        lastWaveTime[msg.sender] = block.timestamp;
+
+        seed = (seed + block.timestamp + block.difficulty) % 100;
+        console.log("Random number geberated : ", seed);
+
+        if (seed < 50){
+            console.log("%s won! ", msg.sender);
+            uint prizeAmount = 0.0001 ether;
+            require(prizeAmount <= address(this).balance, "The contract's balance is insufficient");
+            (bool sent, ) = (msg.sender).call{value: prizeAmount}("");
+            require(sent, "Failed to send eth");
+        }
+    }
+
+    function getAllWaves() public view returns (Wave[] memory){
+        return waves;
+    }
+
+    function getTotalWaves () public view returns(uint) {
+        console.log("We have %d total waves", totalWaves);
         return totalWaves;
     }
 
-    function wave() public {
-        totalWaves++;
-        console.log("%s has waved", msg.sender);
+    function getWavesCount () public view returns(uint) {
+        //console.log("%s waved %d times", msg.sender, waves[msg.sender]);
+        return individualWavesCount[msg.sender];
     }
 }
